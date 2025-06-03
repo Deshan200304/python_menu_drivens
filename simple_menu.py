4ceafdc8a131882a1d6e5d7539ba8acb968e9c62
Deshan200304 <hdganganath003@gmail.com>
2025-06-03 12:16:13 +0530

    display menu

--- simple_menu.py
+++ simple_menu.py
@@ -9,3 +9,10 @@ def multiply(x,y):
 
 def divide(x,y):
     return x / y 
+
+def menu():
+    print("Select operation:")
+    print("1. Add")
+    print("2. Subtract")
+    print("3. Multiply")
+    print("4. Divide")
