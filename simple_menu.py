04933c7370f6faf4f9b128e08d88f192117cf512
Deshan200304 <hdganganath003@gmail.com>
2025-06-04 21:20:19 +0530

    getting numbers from user

--- simple_menu.py
+++ simple_menu.py
@@ -19,4 +19,9 @@ def menu():
 
 while True:
     choice = input("Enter choice(1/2/3/4): ")
+    if choice in ('1', '2', '3', '4'):
+            num1 = float(input("Enter first number: "))
+            num2 = float(input("Enter second number: "))
+
+    
 
